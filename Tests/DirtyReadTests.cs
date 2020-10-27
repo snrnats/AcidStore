f5c114bd739ae1b58fadcfa3872dd8021c12fd42
Dmitry <SnrNatS@live.ru>
2020-10-27 22:37:51 +0300

    Dirty read/write tests

---
+++ Tests/DirtyReadTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store;
+
+namespace Tests
+{
+    [TestClass]
+    public class DirtyReadTests
+    {
+        [TestMethod]
+        public void Get_NewValueIsNotCommitted_OldValueIsRead()
+        {
+            const int oldValue = 100;
+            const int newValue = 200;
+            const int id = 1;
+            var store = new Store<Record>();
+            store.Create(new Record(id, oldValue));
+
+            var transaction = store.BeginTransaction();
+            store.Update(new Record(id, newValue), transaction);
+
+            var actual = store.Get(id);
+
+            store.Commit(transaction);
+
+            Assert.AreEqual(oldValue, actual.Balance);
+        }
+
+        [TestMethod]
+        public void Get_NewValueWillBeAborted_OldValueIsRead()
+        {
+            const int oldValue = 100;
+            const int newValue = 200;
+            const int id = 1;
+            var store = new Store<Record>();
+            store.Create(new Record(id, oldValue));
+
+            var transaction = store.BeginTransaction();
+            store.Update(new Record(id, newValue), transaction);
+
+            var actual = store.Get(id);
+
+            store.Abort(transaction);
+
+            Assert.AreEqual(oldValue, actual.Balance);
+        }
+    }
+}
---
+++ Tests/DirtyWriteTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store;
+
+namespace Tests
+{
+    [TestClass]
+    public class DirtyWriteTests
+    {
+        [TestMethod]
+        public async Task Update_NewValueIsNotCommitted_OldValueIsRead()
+        {
+            const int firstValue = 200;
+            const int secondValue = 300;
+            const int id = 1;
+            var store = new Store<Record>();
+            store.Create(new Record(id, 100));
+
+            var transaction = store.BeginTransaction();
+            store.Update(new Record(id, firstValue), transaction);
+
+            var secondUpdate = Task.Run(() => store.Update(new Record(id, secondValue)));
+
+            var actual = store.Get(id);
+
+            store.Commit(transaction);
+            await secondUpdate;
+
+            Assert.AreEqual(secondValue, actual.Balance);
+        }
+
+        [TestMethod]
+        public async Task Update_NewValueWillBeAborted_OldValueIsRead()
+        {
+            const int firstValue = 200;
+            const int secondValue = 300;
+            const int id = 1;
+            var store = new Store<Record>();
+            store.Create(new Record(id, 100));
+
+            var transaction = store.BeginTransaction();
+            store.Update(new Record(id, firstValue), transaction);
+
+            var secondUpdate = Task.Run(() => store.Update(new Record(id, secondValue)));
+
+            var actual = store.Get(id);
+
+            store.Abort(transaction);
+            await secondUpdate;
+
+            Assert.AreEqual(secondValue, actual.Balance);
+        }
+    }
+}
